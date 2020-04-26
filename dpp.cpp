f0647d7690f6dac8d8ceac8d32247a3cc04adffb
Janez Zemva <janezz55@gmail.com>
2020-04-26 14:04:37 +0200

    some fixes

--- dpp.cpp
+++ dpp.cpp
@@ -2,8 +2,25 @@
 
 #include "dpp.hpp"
 
+template <typename T>
+inline auto sqrt(T const S) noexcept
+{
+  auto xn(S);
+
+  auto const half(dpp::to_decimal<dpp::dec32>(".5"));
+
+  xn = half * (xn + S/xn);
+  xn = half * (xn + S/xn);
+  xn = half * (xn + S/xn);
+
+  return xn;
+}
+
 int main()
 {
+  std::cout << sqrt(dpp::dec32(3)) << std::endl;
+  std::cout << sqrt(dpp::dec32(2)) << std::endl;
+
   auto const a(dpp::to_decimal<dpp::dec32>("1.23"));
   auto const b(dpp::to_decimal<dpp::dec32>("45.6"));
 
