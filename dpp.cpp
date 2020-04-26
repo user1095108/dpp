5cb13e584c1abf0f585a14ec227df74c1f590cb3
Janez Zemva <janezz55@gmail.com>
2020-04-26 13:31:10 +0200

    some fixes

--- dpp.cpp
+++ dpp.cpp
@@ -4,9 +4,6 @@
 
 int main()
 {
-  dpp::dec64 const nan(dpp::dec64::nan{});
-  std::cout << nan.is_nan() << std::endl;
-
   auto const a(dpp::to_decimal<dpp::dec32>("1.23"));
   auto const b(dpp::to_decimal<dpp::dec32>("45.6"));
 
