b14943ee38cc110bff340677cb122616fba532e1
Janez Zemva <janezz55@gmail.com>
2020-04-27 16:30:11 +0200

    some fixes

--- dpp.cpp
+++ dpp.cpp
@@ -3,7 +3,7 @@
 #include "dpp.hpp"
 
 template <typename T>
-constexpr auto sqrt1(T const S) noexcept
+constexpr auto sqrt(T const S) noexcept
 {
   constexpr auto half(dpp::to_decimal<T>(".5"));
 
@@ -20,31 +20,11 @@ constexpr auto sqrt1(T const S) noexcept
   return xn;
 }
 
-template <typename T>
-constexpr auto sqrt2(T const n) noexcept
-{
-  T x;
-  T xn(n);
-
-  do
-  {
-    x = xn;
-    xn = (x + n/x) / 2;
-  }
-  while (x != xn);
-
-  return x;
-}
-
 int main()
 {
-  std::cout << sqrt1(dpp::dec32(2)) << std::endl;
-  std::cout << sqrt2(dpp::dec32(2)) << std::endl;
-  std::cout << sqrt1(dpp::dec32(3)) << std::endl;
-  std::cout << sqrt2(dpp::dec32(3)) << std::endl;
-
-  std::cout << sqrt1(dpp::dec32(9)) << std::endl;
-  std::cout << sqrt2(dpp::dec32(9)) << std::endl;
+  std::cout << sqrt(dpp::dec32(2)) << std::endl;
+  std::cout << sqrt(dpp::dec32(3)) << std::endl;
+  std::cout << sqrt(dpp::dec32(9)) << std::endl;
 
   //
   auto const a(dpp::to_decimal<dpp::dec32>("1.23"));
