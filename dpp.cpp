af6436e5a2f2f8372f8e19efaf2120a24dcc5fd6
Janez Zemva <janezz55@gmail.com>
2020-05-02 14:03:54 +0200

    some fixes

--- dpp.cpp
+++ dpp.cpp
@@ -8,12 +8,18 @@ constexpr auto sqrt(T const S) noexcept
   T x;
   T xn(S);
 
+  T e;
+  T en(S);
+
   do
   {
     x = xn;
     xn = (x + S/x) / T(2);
+
+    e = en;
+    en = xn - x;
   }
-  while (x != xn);
+  while (abs(en) < abs(e));
 
   return xn;
 }
