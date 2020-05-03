59b9e738f6ec9549ed4892b44427229715013885
Janez Zemva <janezz55@gmail.com>
2020-05-03 20:22:33 +0200

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -1156,8 +1156,7 @@ constexpr auto to_decimal(S const& s) noexcept ->
 template <typename T, unsigned M, unsigned E>
 constexpr T to_float(dpp<M, E> const& p) noexcept
 {
-  return isnan(p) ? std::nan("") :
-    p.mantissa() * std::pow(T(10), p.exponent());
+  return isnan(p) ? NAN : p.mantissa() * std::pow(T(10), p.exponent());
 }
 
 template <unsigned M, unsigned E>
