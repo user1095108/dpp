e52cdc10054f7ce4ce15400571c81fd78cb6152d
Janez Zemva <janezz55@gmail.com>
2020-06-10 17:55:48 +0200

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -399,8 +399,7 @@ public:
         --e;
       }
 
-      if (!std::isinf(f) &&
-        (f <= U(std::numeric_limits<std::intmax_t>::max() / 10)) &&
+      if ((f <= U(std::numeric_limits<std::intmax_t>::max() / 10)) &&
         (f >= U(std::numeric_limits<std::intmax_t>::min() / 10)))
       {
         auto tmp(dpp{std::intmax_t(f), 0});
