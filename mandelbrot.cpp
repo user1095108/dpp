0517675de196705100223b4e8d312ac247bf3cae
Janez Zemva <janezz55@gmail.com>
2020-07-19 12:41:33 +0200

    some fixes

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -1,13 +1,14 @@
 // https://github.com/dario-marvin/Mandelbrot
 // https://solarianprogrammer.com/2013/02/28/mandelbrot-set-cpp-11/
-#include <cstdlib>
+#include <sys/ioctl.h> //ioctl() and TIOCGWINSZ
+#include <unistd.h> // for STDOUT_FILENO
 
 #include <iostream>
 
 #include "dpp.hpp"
 
 //using D = float;
-using D = dpp::d32;
+using D = dpp::d64;
 
 constexpr auto limit = 100;
 
@@ -33,31 +34,30 @@ constexpr int mandelbrot(D const r, D const i) noexcept
 
 int main()
 {
-  auto const w(WEXITSTATUS(std::system("exit `tput cols`")));
-  auto const h(WEXITSTATUS(std::system("exit `tput lines`")) - 1);
+  struct winsize ws;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
 
   D const x0(-2), y0(1);
   D const x1(1), y1(-1);
 
-  auto const dx((x1 - x0) / D(w));
-  auto const dy((y1 - y0) / D(h));
+  auto const dx((x1 - x0) / D(ws.ws_col));
+  auto const dy((y1 - y0) / D(ws.ws_row));
 
   auto y(y0 + D(.5) * dy);
 
-  for (int i{}; i != h; ++i, y += dy)
+  for (int i{}; i != ws.ws_row; ++i, y += dy)
   {
     auto x(x0 + D(.5) * dx);
 
-    for (int j{}; j != w; ++j, x += dx)
+    for (int j{}; j != ws.ws_col; ++j, x += dx)
     {
       auto const t(D(mandelbrot(x, y))/D(limit));
       auto const olt(D(1) - t);
 
       std::cout << "\033[48;2;" <<
-        int(D(9)*D(255)*(olt*t*t*t)) << ";" <<
-        int(D(15)*D(255)*(olt*olt*t*t)) << ";" <<
-        int(D(8.5)*D(255)*(olt*olt*olt*t)) << "m" <<
-        " ";
+        int(D(9)*D(255)*(olt*t*t*t)) << ';' <<
+        int(D(15)*D(255)*(olt*olt*t*t)) << ';' <<
+        int(D(8.5)*D(255)*(olt*olt*olt*t)) << "m ";
     }
   }
 
