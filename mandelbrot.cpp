89f811aa8e5155ea305414af17cc45f5f8f13023
Janez Zemva <janezz55@gmail.com>
2020-07-19 11:40:58 +0200

    some fixes

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -5,31 +5,13 @@
 
 #include "dpp.hpp"
 
-constexpr auto char_("o");
-
-constexpr auto black("\033[22;30m");
-constexpr auto red("\033[22;31m");
-constexpr auto l_red("\033[01;31m");
-constexpr auto green("\033[22;32m");
-constexpr auto l_green("\033[01;32m");
-constexpr auto orange("\033[22;33m");
-constexpr auto yellow("\033[01;33m");
-constexpr auto blue("\033[22;34m");
-constexpr auto l_blue("\033[01;34m");
-constexpr auto magenta("\033[22;35m");
-constexpr auto l_magenta("\033[01;35m");
-constexpr auto cyan("\033[22;36m");
-constexpr auto l_cyan("\033[01;36m");
-constexpr auto gray("\033[22;37m");
-constexpr auto white("\033[01;37m");
-
 //using D = float;
 using D = dpp::d32;
 
-constexpr int mb(D const r, D const i) noexcept
-{
-  constexpr auto limit = 100;
+constexpr auto limit = 100;
 
+constexpr int mandelbrot(D const r, D const i) noexcept
+{
   auto zr(r), zi(i);
 
   for (int j{}; j != limit; ++j)
@@ -67,19 +49,28 @@ int main()
 
     for (int j{}; j != w; ++j, x += dx)
     {
-      if (auto const value(mb(x, y)); value == 100) {std::cout << " ";}
-      else if (value > 90) {std::cout << red << char_;}
-      else if (value > 70) {std::cout << l_red << char_;}
-      else if (value > 50) {std::cout << orange << char_;}
-      else if (value > 30) {std::cout << yellow << char_;}
-      else if (value > 20) {std::cout << l_green << char_;}
-      else if (value > 10) {std::cout << green << char_;}
-      else if (value > 5) {std::cout << l_cyan << char_;}
-      else if (value > 4) {std::cout << cyan << char_;}
-      else if (value > 3) {std::cout << l_blue << char_;}
-      else if (value > 2) {std::cout << blue << char_;}
-      else if (value > 1) {std::cout << magenta << char_;}
-      else {std::cout << l_magenta << char_;}
+      if (auto const n(mandelbrot(x, y)); limit == n)
+      {
+        std::cout << " ";
+      }
+      else
+      {
+        auto const t(D(n)/D(limit));
+        auto const t2(t * t);
+
+        auto const olt(D(1) - t);
+        auto const olt2(olt * olt);
+
+        int const c[]{
+          int(D(9)*olt*t2*t*D(255)),
+          int(D(15)*olt2*t2*D(255)),
+          int(D("8.5")*olt2*olt*t*D(255))
+        };
+
+        std::cout << "\033[38;2;" <<
+          c[0] << ";" << c[1] << ";" << c[2] << "m" <<
+          "o";
+      }
     }
   }
 
