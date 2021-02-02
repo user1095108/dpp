8926d2ac7aa027f68f5df97e264f93968dba5669
Janez Žemva <janezz55@gmail.com>
2021-02-02 01:54:17 +0100

    some fixes

--- longint.cpp
+++ longint.cpp
@@ -6,15 +6,25 @@
 
 int main()
 {
-  dpp::longint<std::uint8_t, 2> a(255);
-  dpp::longint<std::uint8_t, 2> b(1);
+  dpp::longint<std::uint8_t, 2> a(4);
+  dpp::longint<std::uint8_t, 2> b(3);
 
+  //
+
+  //
   auto r(a + b);
 
   std::cout << unsigned((a + b)[0]) << std::endl;
   std::cout << unsigned((a - b)[0]) << std::endl;
-  std::cout << (a == b) << std::endl;
-  std::cout << ((a + b) > (a - b)) << std::endl;
+  std::cout << "== " << (a == b) << std::endl;
+  std::cout << "> " << (a > b) << std::endl;
+  std::cout << "< " << (a < b) << std::endl;
 
+  //
+  r = a * b;
+
+  std::cout << unsigned(r[0]) << std::endl;
+
+  //
   return 0;
 }
