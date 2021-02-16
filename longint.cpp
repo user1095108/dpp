44299053258f5e649baedd5cd6e138a1882d475f
Janez Žemva <janezz55@gmail.com>
2021-02-16 04:00:15 +0100

    some fixes

--- longint.cpp
+++ longint.cpp
@@ -6,9 +6,9 @@
 
 int main()
 {
-//using D = longint::longint<std::uint8_t, 8>;
+  using D = longint::longint<std::uint16_t, 8>;
 //using D = longint::longint<std::uint32_t, 4>;
-  using D = longint::longint<std::uint64_t, 2>;
+//using D = longint::longint<std::uint64_t, 2>;
 
   //
   D a(-1025);
@@ -16,8 +16,10 @@ int main()
 
   //
   {
-    std::cout << int(D(-2) >> 1) << std::endl;
-    std::cout << int(D(1) << 11) << std::endl;
+    std::cout << to_raw(D(-11)) << std::endl;
+    std::cout << to_raw(D(-10) + D(-1)) << std::endl;
+    std::cout << int(D(1) << 11) << " : " << to_raw(D(1) << 11) << std::endl;
+    std::cout << int(D(-2) >> 1) << " : " << to_raw(D(-2) >> 1) << std::endl;
   }
 
   //
@@ -31,7 +33,9 @@ int main()
 
   //
   {
-    std::cout << int(a * b) << std::endl;
+    std::cout << int(D(1049600)) << " : " << to_raw(D(1049600)) << std::endl;
+    std::cout << int(a * b) << " : " << to_raw(a * b) << std::endl;
+    //std::cout << (a * b) << std::endl;
   }
 
   //
