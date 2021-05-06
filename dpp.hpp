450190fd769eef8d1710124c298a4f8171cb426c
Janez Žemva <janezz55@gmail.com>
2021-05-06 10:42:24 +0200

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -68,9 +68,7 @@ constexpr auto bit_size() noexcept
 template <typename T, T B>
 constexpr T pow(unsigned const e, T const x = B) noexcept
 {
-  T const y(e % 2 ? x : 1);
-
-  return e ? 1 == e ? x : pow<T, B>(e / 2, x * x) * y : 1;
+  return e ? 1 == e ? x : pow<T, B>(e / 2, x * x) * (e % 2 ? x : 1) : 1;
 }
 
 constexpr int log10(__uint128_t const x, unsigned const e = 0u) noexcept
