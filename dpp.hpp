3eae848374024dd1b8e2481089bbf09e8260ef9f
Janez Žemva <janezz55@gmail.com>
2021-06-01 14:05:53 +0200

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -73,10 +73,8 @@ constexpr void equalize(T const am, int const ae, T& bm, int& be) noexcept
 {
   if (am)
   {
-    for (auto const c(bm >= 0 ? 5 : -5); bm && (be++ != ae);)
-    {
-      bm = (bm + c) / 10;
-    }
+    for (auto const c(bm >= 0 ? 5 : -5); bm && (be++ != ae);
+      bm = (bm + c) / 10);
 
     //
     be = ae;
