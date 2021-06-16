9239e8bafc4f01cb366e40499116b82099fb0777
Janez Žemva <janezz55@gmail.com>
2021-06-16 08:26:41 +0200

    some fixes

---
+++ eps.cpp
@@ -0,0 +1,26 @@
+//https://en.wikipedia.org/wiki/Machine_epsilon#Approximation
+#include <iostream>
+
+#include "dpp.hpp"
+
+template <typename T>
+auto eps() noexcept
+{
+  T e(1);
+
+  while ((1 + e / 2) != 1)
+  {
+    e /= 2;
+  }
+
+  return e;
+}
+
+int main()
+{
+  std::cout << eps<dpp::d16>() << std::endl;
+  std::cout << eps<dpp::d32>() << std::endl;
+  std::cout << eps<dpp::d64>() << std::endl;
+
+  return 0;
+}
