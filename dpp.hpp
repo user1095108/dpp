e0485b2afedffc87b7840e8ab3ab68bff0cefdb0
Janez Žemva <janezz55@gmail.com>
2021-06-19 09:21:34 +0200

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -922,8 +922,7 @@ struct hash<dpp::dpp<M>>
 
         (
           (
-            seed ^= std::hash<std::remove_cvref_t<decltype(v)>>()(
-              std::forward<std::remove_cvref_t<decltype(v)>>(v)) +
+            seed ^= std::hash<decltype(v)>()(std::forward<decltype(v)>(v)) +
               0x9e3779b9 + (seed << 6) + (seed >> 2)
           ),
           ...
