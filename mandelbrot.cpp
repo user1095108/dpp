c275967c5e009ca46ec76962c8a9c501361dd361
Janez Žemva <janezz55@gmail.com>
2021-07-03 07:21:54 +0200

    some fixes

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -50,7 +50,7 @@ int main() noexcept
       CONSOLE_SCREEN_BUFFER_INFO csbi;
       GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
       w = csbi.srWindow.Right-csbi.srWindow.Left + 1;
-      h = csbi.srWindow.Bottom-csbi.srWindow.Top + 1;
+      h = csbi.srWindow.Bottom-csbi.srWindow.Top;
     #else
       struct winsize ws;
       ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
