141172dd2b194d9b6bc3b00223b13357bee8ea62
Janez Žemva <janezz55@gmail.com>
2021-07-09 01:45:21 +0200

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -257,10 +257,10 @@ public:
 
       int e{};
 
-      // eliminate the fractional part, slash f, if necessary
+      // slash f, if necessary, eliminate the fractional part
+      for (; (f < min) || (f > max); f /= U(10), ++e);
       for (; (std::trunc(f) != f) &&
         (f >= min / 10) && (f <= max / 10); f *= U(10), --e);
-      for (; (f < min) || (f > max); f /= U(10), ++e);
 
       *this = {mantissa_type(std::round(f)), e};
     }
