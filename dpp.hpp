81b31a7da5f14399da166c356f40bf45c9eb516e
Janez Žemva <janezz55@gmail.com>
2021-10-29 07:33:30 +0200

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -101,7 +101,7 @@ class dpp
 public:
   using exp_type = std::int16_t;
 
-  enum
+  enum : exp_type
   {
     emin = std::numeric_limits<exp_type>::min(),
     emax = std::numeric_limits<exp_type>::max()
