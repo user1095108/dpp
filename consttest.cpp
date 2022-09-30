6bf4958e09a24d7d37d6658dd2b750b741e9d982
Janez Žemva <janezz55@gmail.com>
2022-09-30 15:48:50 +0200

    some fixes

---
+++ consttest.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "dpp.hpp"
+
+using namespace dpp::literals;
+
+template <auto a>
+void test()
+{
+  std::cout << a << std::endl;
+}
+
+int main()
+{
+  test<dpp::d32{dpp::nan{}}>();
+  test<1 / dpp::d32{3}>();
+  test<.123_d32>();
+  test<0._d32>();
+  test<.0_d32>();
+  test<0_d32 / 0_d32>();
+
+  return 0;
+}
