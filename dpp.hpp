e58f07dfd8a532f0155e192482b5c8e902d8bb12
Janez Žemva <janezz55@gmail.com>
2022-10-20 13:34:42 +0200

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -225,22 +225,15 @@ public:
       }
 
       //
-      enum : int_t
-      {
-        dp = detail::log10((long double)(detail::max_v<mantissa_type>)) - 1
-      };
-
       if (e <= emin)
       {
-        auto const n(std::min(int_t{dp}, emin - e + 1));
-
-        m /= detail::pow<mantissa_type, 10>(n);
-        e += n;
+        v_ = {};
       }
-
-      //
-      v_.m = m;
-      v_.e = e;
+      else
+      {
+        v_.m = m;
+        v_.e = e;
+      }
     }
   }
 
