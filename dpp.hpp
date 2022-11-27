102e59d6666dff660d0933b5492358e76fb7fd41
anon <>
2022-11-27 11:33:42 +0100

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -665,17 +665,17 @@ constexpr T to_decimal(std::input_iterator auto i,
 
     switch (*i)
     {
+      case '0': case '1': case '2': case '3': case '4':
+      case '5': case '6': case '7': case '8': case '9':
+      case '.':
+        break;
+
       case '-':
         neg = true;
         [[fallthrough]];
 
       case '+':
         i = std::next(i);
-        break;
-
-      case '0': case '1': case '2': case '3': case '4':
-      case '5': case '6': case '7': case '8': case '9':
-      case '.':
         break;
 
       default:
