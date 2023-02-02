2dd908e16b4f39e067c03867863566e3937515cb
anon <>
2023-02-02 16:18:10 +0100

    some fixes

--- sqrt.cpp
+++ sqrt.cpp
@@ -15,14 +15,8 @@ int main()
   std::cout << dpp::sqrt(2_d256) << std::endl;
 
   //
-  auto const igr(2 / (1 + dpp::sqrt(5_d256)));
+  std::cout << 2 / (1 + dpp::sqrt(5_d256)) << std::endl;
 
-  std::cout << igr << std::endl;
-
-  std::cout << std::hex << std::uint64_t(igr * (std::uint64_t(1) << 32)) << std::endl;
-  std::cout << std::uint64_t(igr * dpp::detail::pow<dpp::d256, 2>(64)) << std::endl;
-  std::cout << intt::magic::IGR << std::endl;
-  std::cout << intt::intt<std::uint64_t, 3>(igr * dpp::detail::pow<dpp::d256, 2>(128)) << std::endl;
-
+  //
   return 0;
 }
