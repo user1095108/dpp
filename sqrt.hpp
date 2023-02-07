05023f69fd6072d17e6ff16beb8cacaff3f14d3d
anon <>
2023-02-07 14:52:31 +0100

    some fixes

--- sqrt.hpp
+++ sqrt.hpp
@@ -15,12 +15,33 @@ constexpr auto sqrt(intt::intt_type auto m, int_t e) noexcept
 {
   using V = decltype(m);
 
+  if constexpr(V::words > 1)
   {
     constexpr auto e0(
       intt::coeff<
         (1 * V::words / 2) * maxpow10e<typename V::value_type>() - 1
       >()
     );
+
+    e -= e0;
+    m *= intt::coeff<pow<V, 10>(e0)>();
+  }
+  else
+  {
+    static_assert(1 == V::words);
+
+    using U = typename V::value_type;
+    using H = std::conditional_t<
+      std::is_same_v<U, std::uint64_t>,
+      std::uint32_t,
+      std::conditional_t<
+        std::is_same_v<U, std::uint32_t>,
+        std::uint16_t,
+        std::uint8_t
+      >
+    >;
+
+    constexpr auto e0(intt::coeff<maxpow10e<H>() - 1>());
 
     e -= e0;
     m *= intt::coeff<pow<V, 10>(e0)>();
@@ -43,7 +64,14 @@ constexpr auto sqrt(intt::intt_type auto m, int_t e) noexcept
   while (intt::ucompare(m, intt::coeff<V::max() / 5>()) <= 0);
 
   //
-  return dpp<T>(intt::seqsqrt(m), e / 2);
+  if constexpr(V::words > 1)
+  {
+    return dpp<T>(intt::seqsqrt(m), e / 2);
+  }
+  else
+  {
+    return dpp<T>(typename V::value_type(intt::seqsqrt(m)), e / 2);
+  }
 }
 
 }
@@ -52,9 +80,32 @@ template <std::integral T>
 constexpr auto sqrt(dpp<T> const& a) noexcept
 {
   using U = typename std::make_unsigned<T>::type;
-  using V = intt::intt<U, 2>;
 
-  return detail::sqrt<T>(V(intt::direct{}, U(a.mantissa())), a.exponent());
+  if constexpr(std::is_same_v<U, std::uint64_t>)
+  {
+    using V = intt::intt<U, 2>;
+
+    return detail::sqrt<T>(V(intt::direct{}, U(a.mantissa())), a.exponent());
+  }
+  else
+  {
+    using D = std::conditional_t<
+      std::is_same_v<U, std::uint8_t>,
+      std::uint16_t,
+      std::conditional_t<
+        std::is_same_v<U, std::uint16_t>,
+        std::uint32_t,
+        std::conditional_t<
+          std::is_same_v<U, std::uint32_t>,
+          std::uint64_t,
+          void
+        >
+      >
+    >;
+    using V = intt::intt<D, 1>;
+
+    return detail::sqrt<T>(V(intt::direct{}, D(a.mantissa())), a.exponent());
+  }
 }
 
 template <intt::intt_type T>
