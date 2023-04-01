8b79ea7a646e0599f3707842756427eda11bb7fc
anon <>
2023-04-01 11:25:56 +0200

    some fixes

--- tests/sqrt.cpp
+++ tests/sqrt.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include "sqrt.hpp"
+#include "../sqrt.hpp"
 
 using namespace dpp::literals;
 
@@ -10,6 +10,7 @@ int main()
   std::cout << dpp::sqrt(2_d24) << std::endl;
   std::cout << dpp::sqrt(2_d32) << std::endl;
   std::cout << dpp::sqrt(2_d48) << std::endl;
+  std::cout << dpp::sqrt(dpp::to_decimal<dpp::dpp<intt::intt<std::uint8_t, 7>, intt::intt<std::uint8_t, 2>>>("2")) << std::endl;
   std::cout << dpp::sqrt(2_d64) << std::endl;
   std::cout << dpp::sqrt(2_d96) << std::endl;
   std::cout << dpp::sqrt(2_d256) << std::endl;
