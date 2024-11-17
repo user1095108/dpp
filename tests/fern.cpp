9c7d4e5b396de247570ffd21ece273e4beaaba4d
Janez Žemva <janezz55@gmail.com>
2024-11-18 00:44:09 +0100

    some fixes

--- tests/fern.cpp
+++ tests/fern.cpp
@@ -159,14 +159,15 @@ int main(int const argc, char* argv[]) noexcept
   }
 
   auto const mx((xmax + xmin) / 2), my((ymax + ymin) / 2);
+  auto const dh(D(h) / 2), dw(D(w) / 2);
 
   for (auto const& p: points)
   {
     auto const& x(std::get<0>(p)), y(std::get<1>(p));
 
     std::cout << "\033[" <<
-      std::to_string(int((my - y) * sy + D(h) / 2) + 1) << ';' <<
-      std::to_string(int((x - mx) * sx + D(w) / 2) + 1) << "H ";
+      std::to_string(int((my - y) * sy + dh) + 1) << ';' <<
+      std::to_string(int((x - mx) * sx + dw) + 1) << "H ";
   }
 
   std::cout << "\033[0m\033[" << std::to_string(h) << ";1H";
