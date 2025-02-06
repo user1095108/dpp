6e3d2af0406026c00964a26675718989b578b526
Janez Žemva <janezz55@gmail.com>
2025-02-06 17:03:41 +0100

    some fixes

--- dpp.hpp
+++ dpp.hpp
@@ -853,7 +853,7 @@ constexpr T to_decimal(std::input_iterator auto i,
       {
         [[likely]] case '0': case '1': case '2': case '3': case '4':
         case '5': case '6': case '7': case '8': case '9':
-          if ((ar::coeff<T(T::emin + 1)>() < e) &&
+          if ((ar::coeff<typename T::exp_t(T::emin + 1)>() != e) &&
             (r >= ar::coeff<T::mmin / 10>())) [[likely]]
           {
             if (decltype(r) const t(10 * r), d(*i - '0');
