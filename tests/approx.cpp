0406065b7a8c04c42e6fca0dfacc76afc0b5acd3
Janez Žemva <janezz55@gmail.com>
2025-03-27 09:43:37 +0100

    some fixes

--- tests/approx.cpp
+++ tests/approx.cpp
@@ -14,7 +14,8 @@ int main()
 //using D = long double;
   using D = dpp::d128;
 
-  using dpp::midpoint; using std::midpoint;
+  using std::abs; using dpp::abs;
+  using std::midpoint; using dpp::midpoint; 
 
   constexpr auto iterations(100000);
 
@@ -29,26 +30,7 @@ int main()
     std::cout << "Approximation of π: " << 4 / x << std::endl;
   }
 
-  { // π using the Leibniz series
-    D leibniz_pi{};
-
-    std::size_t i(iterations - 1);
-    do { leibniz_pi += (i % 2 ? -1 : 1) / (2 * D(i) + 1); } while (i--);
-
-    std::cout << "Approximation of π: " << 4 * leibniz_pi << std::endl;
-  }
-
-  { // e using the series expansion
-    D e_approximation{};
-    D factorial(1);
-
-    for (std::size_t i(1); i <= iterations; factorial *= ++i)
-      e_approximation += 1 / factorial;
-
-    std::cout << "Approximation of e: " << e_approximation << std::endl;
-  }
-
-  auto const approx([&](auto const f, D const x0 = 1) noexcept
+  auto const approx([&](auto const f, D const& x0 = 1) noexcept
     {
       D x, y{x0}, diff(FLT_MAX);
 
@@ -72,7 +54,68 @@ int main()
   { // the Golden Ratio (φ) using Newton-Raphson for √5
     auto const sqrt5{approx([](auto const x){return midpoint(x, 5 / x);})};
 
+    std::cout << "Approximation of √5: " << sqrt5 << std::endl;
     std::cout << "Approximation of φ (Golden Ratio): " << midpoint(D(1), sqrt5) << std::endl;
+  }
+
+  { // Liouville's constant
+    D liouville{};
+    std::uintmax_t factorial(1);
+
+    for (std::size_t k{1}; k <= 10; factorial *= ++k)
+      liouville += 1 / pow(D(10), factorial);
+
+    std::cout << "Approximation of Liouville's constant: " << liouville << std::endl;
+  }
+
+  {
+    D m, l, h;
+
+    for (l = 1, h = 2; m = midpoint(l, h), (m != l) && (m != h);)
+      if (auto const mr(pow(m, 12)); mr < 2) l = m; else h = m;
+
+    { auto const lr(pow(l, 12)), hr(pow(h, 12)); m = 2 - lr <= hr - 2 ? l : h; }
+
+    std::cout << "Approximation of Twelfth root of two: " << m << std::endl;
+
+    for (l = 1, h = 5; m = midpoint(l, h), (m != l) && (m != h);)
+      if (auto const mr(m * m); mr < 5) l = m; else h = m;
+
+    { auto const lr(l * l), hr(h * h); m = 5 - lr <= hr - 5 ? l : h; }
+
+    std::cout << "Approximation of √5: " << m << std::endl;
+  }
+
+  {
+    std::cout << "Approximation of Plastic ratio: " <<
+      approx([](auto const x){return (2*x*x*x + 1) / (3*x*x - 1);}) << std::endl;
+    std::cout << "Approximation of Golden ratio: " <<
+      approx([](auto const x){return (2*x + 1) / (x + 1);}) << std::endl;
+    std::cout << "Approximation of Silver ratio: " <<
+      approx([](auto const x){return (3*x + 1) / (x + 1);}) << std::endl;
+    std::cout << "Approximation of Supergolden ratio: " <<
+      approx([](auto const x){return (2*x*x*x + 1) / (3*x*x - x);}) << std::endl;
+    std::cout << "Approximation of Supersilver ratio: " <<
+      approx([](auto const x){return (2*x*x*x + 1) / (3*x*x - 2*x);}) << std::endl;
+  }
+
+  { // π using the Leibniz series
+    D leibniz_pi{};
+
+    std::size_t i(iterations - 1);
+    do { leibniz_pi += (i % 2 ? -1 : 1) / (2 * D(i) + 1); } while (i--);
+
+    std::cout << "Approximation of π: " << 4 * leibniz_pi << std::endl;
+  }
+
+  { // e using the series expansion
+    D e_approximation{};
+    D factorial(1);
+
+    for (std::size_t i(1); i <= iterations; factorial *= ++i)
+      e_approximation += 1 / factorial;
+
+    std::cout << "Approximation of e: " << e_approximation << std::endl;
   }
 
   { // ln(2) using the Gregory series
@@ -99,38 +142,6 @@ int main()
     for (std::size_t k{1}; k <= iterations; ++k) apery += 1 / (D(k) * k * k);
 
     std::cout << "Approximation of ζ(3) (Apéry's constant): " << apery << std::endl;
-  }
-
-  { // Liouville's constant
-    D liouville{};
-    std::uintmax_t factorial(1);
-
-    for (std::size_t k{1}; k <= 10; factorial *= ++k)
-      liouville += 1 / pow(D(10), factorial);
-
-    std::cout << "Approximation of Liouville's constant: " << liouville << std::endl;
-  }
-
-  {
-    D m;
-
-    for (D l(1), h(2); m = midpoint(l, h), (m != l) && (m != h);)
-      if (auto const mr(pow(m, 12)); mr < 2) l = m; else h = m;
-
-    std::cout << "Approximation of Twelfth root of two: " << m << std::endl;
-  }
-
-  {
-    std::cout << "Approximation of Plastic ratio: " <<
-      approx([](auto const x){return (2*x*x*x + 1) / (3*x*x - 1);}) << std::endl;
-    std::cout << "Approximation of Golden ratio: " <<
-      approx([](auto const x){return (2*x + 1) / (x + 1);}) << std::endl;
-    std::cout << "Approximation of Silver ratio: " <<
-      approx([](auto const x){return (3*x + 1) / (x + 1);}) << std::endl;
-    std::cout << "Approximation of Supergolden ratio: " <<
-      approx([](auto const x){return (2*x*x*x + 1) / (3*x*x - x);}) << std::endl;
-    std::cout << "Approximation of Supersilver ratio: " <<
-      approx([](auto const x){return (2*x*x*x + 1) / (3*x*x - 2*x);}) << std::endl;
   }
 
   return 0;
