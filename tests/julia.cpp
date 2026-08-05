92f78cf3b69eb7244677a4e9a20afb661c4bbc95
Janez Žemva <janezz55@gmail.com>
2026-08-05 07:57:30 +0200

    some fixes

--- tests/julia.cpp
+++ tests/julia.cpp
@@ -3,10 +3,11 @@
 #define VC_EXTRALEAN
 #include <windows.h>
 #else
-#include <sys/ioctl.h> //ioctl() and TIOCGWINSZ
-#include <unistd.h> // for STDOUT_FILENO
+#include <sys/ioctl.h> // ioctl() and TIOCGWINSZ
+#include <unistd.h> // STDOUT_FILENO
 #endif
 
+#include <cstdlib> // std::atoi
 #include <iostream>
 
 #include "../dpp.hpp"
@@ -48,21 +49,28 @@ int main(int const argc, char* argv[]) noexcept
       auto const handle(GetStdHandle(STD_OUTPUT_HANDLE));
 
       if (DWORD mode; GetConsoleMode(handle, &mode))
-      {
         SetConsoleMode(handle, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+
+      if (CONSOLE_SCREEN_BUFFER_INFO csbi{};
+        GetConsoleScreenBufferInfo(handle, &csbi))
+      {
+        w = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        h = csbi.srWindow.Bottom - csbi.srWindow.Top;
       }
-
-      CONSOLE_SCREEN_BUFFER_INFO csbi;
-      GetConsoleScreenBufferInfo(handle, &csbi);
-      w = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-      h = csbi.srWindow.Bottom - csbi.srWindow.Top;
+      else
+        w = 80, h = 24; // sane fallback
     #else
-      struct winsize ws;
-      ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
-
-      w = ws.ws_col;
-      h = ws.ws_row - 1;
+      if (struct winsize ws{}; -1 != ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws))
+      {
+        w = ws.ws_col;
+        h = ws.ws_row - 1;
+      }
+      else
+        w = 80, h = 24; // sane fallback
     #endif
+
+    if (1 > w) w = 1;
+    if (1 > h) h = 1;
   }
 
   D a, b;
@@ -97,6 +105,61 @@ int main(int const argc, char* argv[]) noexcept
 
       case 6:
         a = {}; b = -.8_d32;
+        break;
+
+      case 7: // c = 1 - phi, phi = golden ratio (~1.618033989); real-valued
+        a = -.61803_d32; b = {};
+        break;
+
+      case 8: // c = (phi - 2) + (phi - 1)i, approx. -0.4 + 0.6i
+        a = -.4_d32; b = .6_d32;
+        break;
+
+      case 9: // c = 0.285 (real-valued)
+        a = .285_d32; b = {};
+        break;
+
+      case 10: // c = -0.70176 - 0.3842i
+        a = -.70176_d32; b = -.3842_d32;
+        break;
+
+      case 11: // c = -0.8 + 0.156i
+        a = -.8_d32; b = .156_d32;
+        break;
+
+      case 12: // c = -0.7269 + 0.1889i
+        a = -.7269_d32; b = .1889_d32;
+        break;
+
+      case 13: // c = 0.35 + 0.35i
+        a = .35_d32; b = .35_d32;
+        break;
+
+      case 14: // c = 0.4 + 0.4i
+        a = .4_d32; b = .4_d32;
+        break;
+
+      // The following four are the classic named quadratic Julia sets
+      // (Weisstein, "Julia Set," MathWorld)
+
+      case 15: // Dendrite fractal: c = i; a Misiurewicz point where the
+               // Julia set degenerates into a branched, thread-like curve
+        a = {}; b = 1_d32;
+        break;
+
+      case 16: // Douady's rabbit fractal: c = -0.123 + 0.745i
+        a = -.123_d32; b = .745_d32;
+        break;
+
+      case 17: // San Marco fractal: c = -0.75 (real-valued); named for its
+               // resemblance to the domes of St. Mark's Basilica in Venice
+        a = -.75_d32; b = {};
+        break;
+
+      case 18: // Siegel disk fractal: c = -0.39054 - 0.58679i; contains a
+               // Siegel disk, an invariant region on which the map acts
+               // as an irrational rotation
+        a = -.39054_d32; b = -.58679_d32;
         break;
     }
   }
